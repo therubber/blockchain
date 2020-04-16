a6d240b947525afde1cf277837c84e3ad64ac3c1
ruben <ruben.leibmann@gmx.de>
2020-04-16 18:33:56 +0200

    Configured production code

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 
-const PORT = process.env.port || PEER_PORT || DEFAULT_PORT;
+const PORT = process.env.PORT || PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`listening at localhost:${PORT}`);
     if (PORT !== DEFAULT_PORT) {
